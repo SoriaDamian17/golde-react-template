b51af7984746f8ee9bf53245001c773a77d94cc0
Damian Soria <damian.soria@youbim.com>
2020-03-23 12:21:46 -0300

    fix: update styles

--- src/pages/home.js
+++ src/pages/home.js
@@ -8,7 +8,12 @@ export default () => {
     return (
         <Layout title="Home">
             <div className="main-container">
-
+                <div className="col col-1">
+                    <h2>Optimization</h2>
+                    <p>
+                        Bundles very small and optimization of code.
+                    </p>
+                </div>
             </div>
         </Layout>
     )
