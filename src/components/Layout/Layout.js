c12c2ca9ab0831ae974e5725f07f0768873eee92
Damian Soria <damian.soria@youbim.com>
2020-03-22 21:59:02 -0300

    feat: add new component and asset

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,6 +1,8 @@
 import React, {useEffect} from 'react';
 import './Layout.scss';
 
+import Logo from '../Logo';
+
 const Layout = (props) => {
 
     useEffect(() => {
@@ -9,6 +11,11 @@ const Layout = (props) => {
 
     return (
         <div className="layout">
+            <div className="main-header">
+                <Logo width={250} height={250}/>
+                <h1 className="title">React Template</h1>
+                <p className="subtitle">A JavaScript library for building user interfaces</p>
+            </div>
             {props.children}
         </div>
     )
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Layout from '../components/Layout';
 
+import '../styles/home.scss';
+
 export default () => {
 
     return (
         <Layout title="Home">
-            <h1>Hi Pokemon</h1>
+            <div className="main-container">
+
+            </div>
         </Layout>
     )
 }
